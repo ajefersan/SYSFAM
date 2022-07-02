7b91cf8a23992ea3e6c6118ea233db50f67d938a
glaub <gau1210@gmail.com>
2022-07-02 11:15:44 -0300

    Cadastro Usuario OK

--- app/src/main/java/com/example/sysfam/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/com/example/sysfam/activity/CadastroUsuarioActivity.java
@@ -1,13 +1,20 @@
 package com.example.sysfam.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.sysfam.R;
+import com.example.sysfam.config.ConfiguracaoFirebase;
 import com.example.sysfam.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
@@ -16,6 +23,8 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
     private EditText senha;
     private Button botaoCadastrar;
     private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +36,28 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         senha = (EditText) findViewById(R.id.edit_cadastro_senha);
         botaoCadastrar = (Button) findViewById(R.id.bt_cadastrar);
 
-        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
+        botaoCadastrar.setOnClickListener((v) -> {
                 usuario = new Usuario();
                 usuario.setNome(nome.getText().toString());
                 usuario.setEmail(email.getText().toString());
                 usuario.setSenha(senha.getText().toString());
                 cadastrarUsuario();
-            }
         });
     }
     private void cadastrarUsuario(){
-
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(CadastroUsuarioActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(CadastroUsuarioActivity.this,"Sucesso ao cadastrar usuário", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(CadastroUsuarioActivity.this,"Erro ao cadastrar usuário", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
