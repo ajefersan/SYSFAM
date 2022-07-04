b409b215956709e03a56765c26a8641e074aa026
glaub <gau1210@gmail.com>
2022-07-03 23:05:49 -0300

    Criação do Toolbar

--- app/src/main/java/com/example/sysfam/activity/MainActivity.java
+++ app/src/main/java/com/example/sysfam/activity/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.sysfam.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +17,7 @@ import com.example.sysfam.R;
 import com.example.sysfam.config.ConfiguracaoFirebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.internal.ToolbarUtils;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -27,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button botaoSair;
     private FirebaseAuth autenticacao;
-
+    private DatabaseReference firebase;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +41,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         botaoSair = (Button) findViewById(R.id.bt_sair);
-
         botaoSair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
                 autenticacao.signOut();
-
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
+        firebase = ConfiguracaoFirebase.getFirebase();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("SYSFAM");
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+
     }
 }
