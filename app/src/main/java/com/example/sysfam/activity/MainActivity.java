9c2c566e2108bacea334c4b87f4ae5732ef73cec
glaub <gau1210@gmail.com>
2022-07-04 22:08:02 -0300

    Mudando para Drower Navegação atualizado 2.0

--- app/src/main/java/com/example/sysfam/activity/MainActivity.java
+++ app/src/main/java/com/example/sysfam/activity/MainActivity.java
@@ -41,19 +41,13 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_pricipal,R.id.nav_servico,R.id.nav_sobre,
+                R.id.nav_sair)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navegacao);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navegacao, menu);
-        return true;
     }
 
     @Override
